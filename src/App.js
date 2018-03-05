9fb02fd845457716fe8181b79bd712389655912d
Luis Rojas <lrojas94@gmail.com>
2018-03-04 22:20:22 -0400

    Add FB login button

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FacebookLogin from 'react-facebook-login'
+
 class App extends Component {
+
+  loggedIntoFb = (response) => {
+    console.log(response);
+  }
+
   render() {
     console.log(process.env);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">AWS STS React Example</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <FacebookLogin
+            appId={process.env.REACT_APP_FB_APP_ID}
+            autoLoad={true}
+            callback={this.loggedIntoFb} 
+          />      
         </p>
       </div>
     );
